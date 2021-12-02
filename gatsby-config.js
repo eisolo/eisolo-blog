91db5499301b799fb10089ae0cf5fd021f816989
eisolo <slonik@gmail.com>
2021-12-01 20:45:35 -0800

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,12 @@
 module.exports = {
   siteMetadata: {
     // edit below
-    title: `Gatsby Starter Personal Blog`,
-    author: `Gatsby`,
-    description: `A starter personal blog with styled components, dark mode, and Netlify CMS.`,
+    title: `eisolo+gatsby+netlify`,
+    author: `eisolo`,
+    description: `the quick brown fox jumps over the lazy dog`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
     social: {
-      twitter: `gatsbyjs`,
+      twitter: ``,
     },
   },
   plugins: [
