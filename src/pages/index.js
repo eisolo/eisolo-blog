71b77beb33c7e6feb9e7394965ac20fa62542773
eisolo <slonik@gmail.com>
2021-12-02 01:17:52 -0800

    index update

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,21 +15,11 @@ class IndexPage extends React.Component {
           title="Home"
           keywords={[`blog`, `gatsby`, `javascript`, `react`]}
         />
-        <img style={{ margin: 0 }} src="./GatsbyScene.svg" alt="Gatsby Scene" />
-        <h1>
-          Hey people{" "}
-          <span role="img" aria-label="wave emoji">
-            👋
-          </span>
-        </h1>
-        <p>Welcome to your new Gatsby website. You are on your home page.</p>
-        <p>
-          This starter comes out of the box with styled components and Gatsby's
-          default starter blog running on Netlify CMS.
-        </p>
-        <p>Now go build something great!</p>
+      
+        <p>home is where this text is</p>
+    
         <Link to="/blog/">
-          <Button marginTop="35px">Go to Blog</Button>
+          <Button marginTop="10px">Go to Blog</Button>
         </Link>
       </Layout>
     )
